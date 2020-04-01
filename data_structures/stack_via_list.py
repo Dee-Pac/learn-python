4a8039c6a452606b069ae8cf03ac0fbcc73beb78
Deepak MC <mcdeepak@gmail.com>
2020-04-01 00:02:07 -0700

    [Stack] Implement Stack via list - add more functions

--- data_structures/stack_via_list.py
+++ data_structures/stack_via_list.py
@@ -13,15 +13,21 @@ class Stack:
     def peek(self):
         return self.items[-1]
     
+    def length(self):
+        return len(self.items)
+    
+    def is_empty(self):
+        return self.items == []
+    
     def __repr__(self):
         return str(self.items)
         
-stack = Stack()
-stack.push(1).push(2).push(3)
-print(stack.pop(),stack)
+# stack = Stack()
+# stack.push(1).push(2).push(3)
+# print("Popped -> {} | Stack -> {} | Length -> {} | Empty? -> {}".format(stack.pop(),stack, stack.length(), stack.is_empty()))
 # 3 [1, 2]
-print(stack.pop(),stack)
+# print("Popped -> {} | Stack -> {} | Length -> {} | Empty? -> {}".format(stack.pop(),stack, stack.length(), stack.is_empty()))
 # 2 [1]
-print(stack.peek())
+# print(stack.peek())
 # 1
 
