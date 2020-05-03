dbc0c245fc1bccd98d3747fa92ea295dc22f8be5
Deepak Chandramouli <mcdeepak@gmail.com>
2020-05-03 06:27:22 -0700

    [Generator] Update generator with more usage (#14)

--- utils/enumeration_generator.py
+++ utils/enumeration_generator.py
@@ -24,4 +24,36 @@ for item in enumerate(l,1):
 for items in gen_seq(l,1):
     print(type(items),items)    
     
+items = gen_seq(l,1)
+
+print(next(items))
+# (1, 'a')
+print(next(items))
+# (2, 'b')
+print(next(items))
+# (3, 'c')
+print(next(items))
+# (4, 'd')
+# print(next(items))
+# Traceback (most recent call last):
+#   File "/Users/dmohanakumarchan/workspace/eclipse_luna/learn-python/test.py", line 25, in <module>
+#     print(next(items))
+# StopIteration
+
+i= [1,2,3]
+# square each term using list comprehension
+l = [x**2 for x in i]
+# same thing can be done using generator expression
+g = (x**2 for x in l)
+print(l)
+# [1, 4, 9]
+print(g)
+# <generator object <genexpr> at 0x10eadf960>
+print(next(g))
+# 1
+print(next(g))
+# 16
+
+# More https://www.programiz.com/python-programming/generator
+
 
