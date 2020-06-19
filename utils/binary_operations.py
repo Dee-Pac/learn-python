3376a6c2174596b3e56a3022cca0defa10553d4e
Deepak Chandramouli <mcdeepak@gmail.com>
2020-06-19 07:35:34 -0700

    [Utils] Add Binary to Int converter

--- utils/binary_operations.py
+++ utils/binary_operations.py
@@ -22,3 +22,28 @@ def get_binary(num):
 
 # print(get_binary(8))
 # 1000
+
+import math
+def get_int(bin_string):
+    base = 2
+    res = 0
+    for bit in enumerate(bin_string[::-1]):
+        bit_position = bit[0]
+        bit_value = bit[1]
+        print(bit_position,bit_value)
+        res += int(math.pow(base,bit_position)) * int(bit_value)
+    return res
+
+print(get_int("1000"))
+# 0 0
+# 1 0
+# 2 0
+# 3 1
+# 8
+
+print(get_int("0111"))
+# 0 1
+# 1 1
+# 2 1
+# 3 0
+# 7
