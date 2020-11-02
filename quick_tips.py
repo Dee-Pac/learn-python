7e08853e3921e2d66251bb67e87c530372ed6274
Deepak Chandramouli <mcdeepak@gmail.com>
2020-11-01 18:19:36 -0800

    Update quick_tips.py

--- quick_tips.py
+++ quick_tips.py
@@ -204,11 +204,6 @@ writer.write(data_string + "\n")
 writer.writelines(lines)
 writer.close()
 
-## Regex
-
-## URL parsing
-
-## Time & Date
 
 print("+++++++++++++++++++++++++++++++++++++")
 print(" ---- CLASS INHERITENCE  -----")
@@ -273,9 +268,54 @@ k = json.load(fp)
 print(k)
 fp.close()
 
-## Multithreading, Multiprocessing
+print("+++++++++++++++++++++++++++++++++++++")
+print(" ---- THREADING  -----")
+print("+++++++++++++++++++++++++++++++++++++")
+
+from threading import Lock,Thread,current_thread
+import threading
+
+class Counter:
+    def __init__(self,counter=0):
+        self.counter = counter
+        self.lock = Lock()
+
+    def count(self):
+        with self.lock:
+            # self.lock.acquire() # Alternate for with lock:
+            print(current_thread().getName(),self.counter)
+            for k in range(100000):
+                self.counter +=1
+            # self.lock.release()
+
+# initiate counter object
+obj = Counter()
+
+# initiate threads
+threads = []
+
+# create 40 threads for count function
+for k in range(40):
+    new_thread = Thread(target = obj.count,args = ())
+    threads.append(new_thread)
+
+# start the threads
+for thread in threads:
+    thread.start()
+
+# wait for completion of all threads
+for thread in threads:
+    thread.join()
+
+# Without above join calls, the print could execute even before all the counter functions completed.
+print(obj.counter)
 
 
+## Regex
+
+## URL parsing
+
+## Time & Date
 
 import sqlite3
 
