88c70e80676871397acb1dd4d5a3bf90e77c454b
Deepak Chandramouli <mcdeepak@gmail.com>
2020-11-01 18:22:48 -0800

    Update quick_tips.py

--- quick_tips.py
+++ quick_tips.py
@@ -269,7 +269,7 @@ print(k)
 fp.close()
 
 print("+++++++++++++++++++++++++++++++++++++")
-print(" ---- THREADING  -----")
+print(" ---- THREADING , LOCK, RE-ENTRANT LOCK  -----")
 print("+++++++++++++++++++++++++++++++++++++")
 
 from threading import Lock,Thread,current_thread
@@ -310,6 +310,48 @@ for thread in threads:
 # Without above join calls, the print could execute even before all the counter functions completed.
 print(obj.counter)
 
+###---------------------- RE-ENTRANT CODE ----------------------###
+
+# A Re-entrant code is fully atomic in the sense - it does not modify any object outside of its scope.
+
+def pow(num):
+    return num*num
+
+# Non Re-entrant code
+# Not thread safe without an explicit lock
+k = 10
+def pow1(num):
+    global k 
+    k +=1
+    return num*num
+
+###---------------------- RE-ENTRANT LOCK ----------------------###
+
+# Ability of a thread to re-acquire lock in something it already had (locked)
+
+from threading import *
+
+lock = Lock()
+k = lock.acquire()
+print("first locked {}".format(k))
+k = lock.acquire(timeout=0)
+print("second locked {}".format(k))
+lock.release()
+
+# first locked True
+# second locked False
+
+from threading import *
+
+lock = RLock()
+k = lock.acquire()
+print("first locked {}".format(k))
+k = lock.acquire()
+print("second locked {}".format(k))
+lock.release()
+
+# first locked True
+# second locked True
 
 ## Regex
 
