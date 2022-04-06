8feff4582cfbe02a258c6c63078f2ffe1b9f30cb
Dee-Pac <mcdeepak@gmail.com>
2022-04-06 12:32:45 -0700

    Add binary search and github

--- problems/binary_search.py
+++ problems/binary_search.py
@@ -1,167 +1,158 @@
+from typing import List
+import unittest
+from unittest import TestCase
+import sys
 
-# -- Simple Binary Search of sorted array
+class BinarySearch(object):
 
-def find(nums, num):
-    
-    size = len(nums)
-    start = 0
-    end = size - 1
-    
-    found = None
-    
-    while (start <= end):
-        
-        offset = (end - start)//2
-        middle = start + offset
-        
-        if ( nums[middle] == num ):
-            found = middle
-            break
-        else:
-            if ( num > nums[middle] ):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def firstNotSmaller(self, nums: List[int], target: int) -> int:
+
+        start = 0
+        end = len(nums)-1
+        res = -1
+
+        while start <= end:
+            middle = (start + end) // 2
+            if (nums[middle] >= target):
+                res = middle
+                end = middle - 1
+            else:
+                start = middle + 1
+
+        return res
+
+    @classmethod
+    def firstOccurrence(self, nums: List[int], target: int) -> int:
+
+        start = 0
+        end = len(nums) - 1
+        res = -1
+
+        while start <= end:
+            middle = (start + end) // 2
+            
+            if (target <= nums[middle]):
+                if (target == nums[middle]):
+                    res = middle
+                end = middle - 1
+            else:
+                start = middle + 1
+        return res
+
+    @classmethod
+    def squareRoot(self, target: int) -> int:
+
+        if (target <= 1):
+            return target
+
+        res = None
+        start = 0
+        end = target
+
+        while (start <= end):
+            middle = (start + end) // 2
+            if (middle * middle > target):
+                end = middle - 1
+            else:
+                res = middle
+                start  = middle + 1
+
+        return res
+
+    @classmethod
+    def minSortedRotatedArray(self, nums: List[int]) -> int:
+
+        start = 0
+        end = len(nums) - 1
+        res = -1 * sys.maxsize
+
+        while start <= end:
+
+            middle = (start + end) // 2
+            res = middle
+            if (nums[start] <= nums[middle]):
                 start = middle + 1
             else:
                 end = middle - 1
-        
-    if (found):
-        print(found)
-    else:
-        print("Not Found")
-        
+
+        return res
+
+
+
+class TestBinarySearch(TestCase):
+
+    def setUp(self):
+
+        self.search = BinarySearch()
+
+    def testFirstNotSmaller(self):
+
+        testSetFirstNotSmaller = [
+            ([], 1, -1),
+            ([1,2,3], 2, 1),
+            ([1,2,2,4], 3, 3),
+            ([1,2,3], 4, -1)
+        ]
+
+        for testInputs in testSetFirstNotSmaller:
+            nums = testInputs[0]
+            target = testInputs[1]
+            expectedResult = testInputs[2]
+            self.assertEqual(self.search.firstNotSmaller(nums, target), expectedResult, "testFirstNotSmaller | {}".format(testInputs))
+
+    def testFirstOccurrence(self):
+
+        testSetFirstOccurrence = [
+            ([], 5, -1),
+            ([1,2,2,3,4,5,6,7], 2, 1),
+            ([1,2,3,4,5,6,7,8],8,7),
+            ([0,1,2,3], 5, -1),
+            ([0,1,2,3], -1, -1)
+        ]
+
+        for testInputs in testSetFirstOccurrence:
+            nums = testInputs[0]
+            target = testInputs[1]
+            expectedResult = testInputs[2]
+            self.assertEqual(self.search.firstOccurrence(nums, target), expectedResult, "testFirstOccurrence | {}".format(testInputs))
+
+    def testSquareRoot(self):
+
+        testSetSquareRoot = [
+            (0,0),
+            (8,2),
+            (10,3),
+            (1,1)
+        ]
+
+        for inputs in testSetSquareRoot:
+            target = inputs[0]
+            expected = inputs[1]
+            self.assertEqual(self.search.squareRoot(target),expected,"testSquareRoot | {}".format(inputs))
+
+    def testMinSortedRotatedArray(self):
+
+        testSetMinSortedRotatedArray = [
+            ([], -1 * sys.maxsize),
+            ([2,1], 1),
+            ([10,20,30,2], 3),
+            ([-10, 0, 10, -20], 3)
+        ]
+
+        for inputs in testSetMinSortedRotatedArray:
+            nums = inputs[0]
+            expected = inputs[1]
+            self.assertEqual(self.search.minSortedRotatedArray(nums), expected, "testMinSortedRotatedArray | {}".format(inputs))
+
+    def tearDown(self):
+
+        del self.search
+        print("ALL TEST CASES PASSED")
+
     
-# find([1,2,3,4,5,6],10)
-
-
-# -- Find First or last index of an element
-
-nums = [2, 3, 10, 10, 10, 10, 18, 20]
-
-def findFirstOrLastIter(nums, num, first = True):
-    
-    result = None
-    
-    size = len(nums)
-    start = 0
-    end = size - 1
-    
-    
-    while (start <= end):
-        
-        offset = (end - start) // 2
-        middle = start + offset
-        
-        if (num == nums[middle]):
-            result = middle
-            if (first): end = middle - 1
-            else: start = middle + 1
-        elif (num < nums[middle]):
-            end = middle - 1
-        else:
-            start = middle + 1
-    
-    return result
-
-# print(findFirstOrLastIter(nums, 10))
-# print(findFirstOrLastIter(nums, 200))
-# print(findFirstOrLastIter(nums, 3))
-# print(findFirstOrLastIter([], 2))
-# print(findFirstOrLastIter(nums, 2))
-# print(findFirstOrLastIter(nums, 20))
-
-
-# -- FIND ALL OCURRENCES OF AN ELEMENT
-
-def findAll(nums, num):
-    
-    first = findFirstOrLastIter(nums, num)
-    last = findFirstOrLastIter(nums, num, False)
-    
-    return (first, last)
-
-    
-print(findAll(nums, 3))
-print(findAll(nums, 10))
-print(findAll([10,10], 10))
-
-
-
-
-
-# -- Given sorted array that is rotated, find how many times rotated
-# -- Solution : the middle is the lowest & its index is the rotated times.
-
-def howManyRotated(nums):
-    
-    size = len(nums)
-    start = 0
-    end = size-1
-    
-    while (start <= end):
-        
-        offset = (end-start) // 2
-        middle = start + offset
-        # print(start, middle, end, middle+1)
-        if (nums[middle] > nums[end]):
-            # print("a")
-            start = middle + 1
-        elif (nums[start] > nums[middle - 1]):
-            # print("b")
-            end = middle - 1
-        else:
-            break
-            
-    print(middle)
-    
-nums1 = [3,4,5, 1,2]
-nums2 = [6,1,2,3,4,5]
-nums3 = [6,7,8,9,1,2,3,4,5]
-
-        
-# howManyRotated(nums1)
-# howManyRotated(nums2)
-# howManyRotated(nums3)
-
-                
-def findElementInSortedRotatedArray(nums, num):
-    
-    size = len(nums)
-    end = size - 1
-    start = 0
-    
-    while (start <= end):
-        
-        offset = (end-start)//2
-        middle = start + offset
-        
-        # print(start, end, middle)
-        
-        if (num == nums[middle]):
-            return middle
-        
-        isLeftSorted = nums[start] <= nums[middle-1]
-        isRightSorted = nums[middle+1] <= nums[end]
-        isNumInLeftRange = num >= nums[start] and num <= nums[middle-1]
-        isNumInRightRange = num >= nums[middle+1] and num <= nums[end]  
-        
-        if (isLeftSorted):
-            if isNumInLeftRange:
-                end = middle-1
-            else:
-                start = middle+1
-        else:
-            if isNumInRightRange:
-                start = middle+1
-            else:
-                end= middle-1
-
-        # print(start, end, middle)
-            
-    return -1
-
-# print(findElementInSortedRotatedArray([6,1,2,3,4,5],4))
-# print(findElementInSortedRotatedArray([6,1,2,3,4,5],1))
-# print(findElementInSortedRotatedArray([6,7,8,9,1],1))                
-
-            
-
-
+if __name__ == "__main__":
+    unittest.main()
